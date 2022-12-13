9e58f03c413988c4eb68a8e16ea9bfe86f249dc7
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-13 18:27:35 +0530

    ADDED : Code to Analyse Mood.

---
+++ src/main/java/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package moodanalyser;
+
+public class MoodAnalyser {
+    public String analyseMood(String message) {
+        if(message.contains("Sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.moodanalyser;
+
+import moodanalyser.MoodAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Happy Meassage");
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
